787fe47628cc4bdbba3d8f82ced7382ab9eedf52
Edwin Wang <edwinwang62@gmail.com>
2025-07-18 00:38:56 +0300

    Update

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main(){
+    let mut fname="Wangs";
+    let mut lname="Corporation";
+    println!("Hello {fname} {lname}");
+    fname="Edwin";
+    lname="Wanyoike";
+    println!("Hello {fname} {lname}");
+}
